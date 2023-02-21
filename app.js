2eb289113f60f22e6a3960b99430d9d79431d8cd
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-21 10:07:20 +0530

    basic model Schema

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
-const app =  express();
 const path =  require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
+
+mongoose.set('strictQuery', false);
+    main().catch(err => console.log(err));
+    async function main() { await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+        .then(() => {
+        console.log('Database Connected')
+        })
+        .catch(err => {
+            console.log('OH no !! Mongoose ErroR!!!');
+            console.log(err)
+    })
+    }
+const app =  express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/makecampgrounds', async(req, res) => {
+    const camp = new Campground({title: 'My Backyard' , description : 'Cheap Camping'})
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {
