b0d917770b1270d21c479c8465497cd3d74b4aba
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-21 12:46:42 +0530

    Campground Edit and Update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path =  require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const campground = require('./models/campground');
 
 
 mongoose.set('strictQuery', false);
@@ -20,6 +21,8 @@ const app =  express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded( { extended : true} ))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -27,6 +30,16 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index' , { campgrounds})
+})
+
+app.get('/campgrounds/new' , (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds' , async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/:id' , async(req, res) => {
