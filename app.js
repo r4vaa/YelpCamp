f3417c00c554d02abab8ca1962c549f1c5d6d8c3
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-21 17:32:30 +0530

    edit and update campground

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path =  require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 const campground = require('./models/campground');
 
 
@@ -22,6 +23,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded( { extended : true} ))
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -45,6 +47,17 @@ app.post('/campgrounds' , async(req, res) => {
 app.get('/campgrounds/:id' , async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
+})
+
+app.get('/campgrounds/:id/edit', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit' ,{ campground});
+})
+
+app.put('/campgrounds/:id' , async( req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground} );
+    res.redirect(`/campgrounds/${campground._id}`);
 })
 
 app.get('/makecampgrounds', async(req, res) => {
