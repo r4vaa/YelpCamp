91e702bfb4063e7fb91baf5fc6798452187e5886
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-03 11:46:57 +0530

    adding images to our campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -25,9 +25,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50 ;i ++){
         const random1000 = Math.floor(Math.random() *1000);
+        const price = Math.floor(Math.random() *20) +10 ;
         const camp = new Campground(  {
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title : `${sample(descriptors)} ${sample(places)}`
+            title : `${sample(descriptors)} ${sample(places)}`,
+            image : 'https://source.unsplash.com/collection/483251',
+            description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus placeat ab totam, officiis recusandae voluptatem reiciendis ducimus voluptatum facere fugit molestiae vitae sunt perspiciatis veritatis architecto esse eaque aspernatur maxime?',
+            price
         })
         await camp.save();
 
