acd63c728989bfb7b31a0f42330133beffdd7297
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-15 15:28:39 +0530

    Register Route logic

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/fakeUser' , async(req, res) => {
-    const user = new User({ email: 'patil56@gmail.com', username:'dhee'});
+    const user = new User({ email: 'patilss56@gmail.com', username:'dhees'});
     const newUser = await User.register(user, 'chicken');
     res.send(newUser)
 })
--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const UserSchema = new Schema ( {
             required : true,
             unique : true
         }
-})
+});
 
 UserSchema.plugin(passportLocalMongoose);
 
