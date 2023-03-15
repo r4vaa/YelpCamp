98edac703bc81b4fb2fc12e7062acb66268d3083
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-15 10:37:20 +0530

    configuring passport

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const session = require('express-session');
 const ExpressError = require('./utils/ExpressError')
 const flash = require('connect-flash');
 const methodOverride = require('method-override');
-const Joi = require('joi')
+const Joi = require('joi');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const usersRoutes = require('./routes/users')
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
 
 
 
@@ -47,14 +51,30 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
-app.use('/campgrounds' , campgrounds);
-app.use('/campgrounds/:id/reviews' , reviews);
+app.get('/fakeUser' , async(req, res) => {
+    const user = new User({ email: 'patil56@gmail.com', username:'dhee'});
+    const newUser = await User.register(user, 'chicken');
+    res.send(newUser)
+})
+
+
+app.use('/',usersRoutes);
+app.use('/campgrounds' , campgroundsRoutes);
+app.use('/campgrounds/:id/reviews' , reviewsRoutes);
 
 app.get('/', (req, res) => {
     res.render('home')
