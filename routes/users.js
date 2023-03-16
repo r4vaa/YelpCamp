e9df7a966e64d881571c2808402ea4f3bb1e395b
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-16 14:08:18 +0530

    ReturnTo Behavior

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user');
 const passport = require('passport');
-
+const { checkReturnTo} = require('../middleware');
 
 router.get('/register' , (req ,res ) => {
     res.render('users/register')
@@ -29,9 +29,11 @@ router.get('/login', (req, res) => {
     res.render('users/login')
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}),(req, res) => {
+router.post('/login', checkReturnTo,passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}),(req, res) => {
     req.flash('success','Welcome Back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl); 
 })
 
 
