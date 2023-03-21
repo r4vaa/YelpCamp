f91160c250142d9c1a732fdf8f9415e64eb818a2
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-21 17:44:13 +0530

    adding upload to edit page

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -46,6 +46,9 @@ module.exports.renderEditForm = async(req, res, next) => {
 module.exports.updateCampground = async( req, res, next) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground} );
+    const imgs =  req.files.map(f => ({url: f.path , filename: f.filename}));
+    campground.images.push(...imgs);
+    await campground.save()
     req.flash('success', 'Successfully update campground!')
     res.redirect(`/campgrounds/${campground._id}`);
 }
