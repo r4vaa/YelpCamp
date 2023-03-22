9ab1a4215ea0ec79d6b66d42f17efe7b50277793
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-22 18:42:25 +0530

    customizing Map Popup

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -1,7 +1,9 @@
+// const campground = require("../../models/campground");
+
  mapboxgl.accessToken = mapToken;
       const map = new mapboxgl.Map({
       container: 'map', // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      style: 'mapbox://styles/mapbox/dark-v10', // style URL
       center: campground.geometry.coordinates, // starting position [lng, lat]
       zoom: 9, // starting zoom
       });
@@ -9,4 +11,10 @@
 
 new mapboxgl.Marker()
       .setLngLat(campground.geometry.coordinates)
+      .setPopup(
+          new mapboxgl.Popup({ offset: 25 })
+          .setHTML(
+               `<h3>${campground.title}</h3><p>${campground.location}</p>`
+          )
+      )
       .addTo(map)
