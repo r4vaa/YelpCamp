34621a70c50a2ef1e5bb7245ed68bb8ad16efbf4
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-23 16:43:46 +0530

    chaning ports

--- app.js
+++ app.js
@@ -181,8 +181,9 @@ app.use((err, req, res , next) =>{
     
 })
 
-app.listen(3000, () => {
-    console.log('Serving on Port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on Port ${port}`)
 })
 
 
